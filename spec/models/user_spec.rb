39e666b49cbd0da07c68c495841e4a5e18972a4c
Markus Nordin <markus.nordin@gmail.com>
2017-06-17 12:59:57 +0200

    Add missing unit test
    Properly set up factory girl as well. My bad.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
   end
+
+  describe '#name' do
+    it 'combines first name and last name' do
+      user = build(:user, first_name: 'First', last_name: 'Last')
+
+      expect(user.name).to eq('First Last')
+    end
+  end
 end
