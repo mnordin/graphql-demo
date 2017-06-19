306e7862cc076d49696326f3fa1a80d90e446f14
Markus Nordin <markus.nordin@gmail.com>
2017-06-19 19:13:16 +0200

    Show a rich example query when first opening the app

---
+++ config/initializers/graphiql.rb
@@ -0,0 +1,33 @@
+GraphiQL::Rails.config.initial_query = <<-EOS
+# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser IDE for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will
+# see intelligent typeaheads aware of the current GraphQL type schema and
+# live syntax and validation errors highlighted within the text.
+#
+# To bring up the auto-complete at any point, just press Ctrl-Space.
+#
+# Press the run button above, or Cmd-Enter to execute the query, and the result
+# will appear in the pane to the right.
+#
+# Here is an example query you might enjoy running:
+
+query {
+  user(id: 1) {
+    name
+    posts {
+      title
+      content
+      comments {
+        user {
+          name
+        }
+        content
+      }
+    }
+  }
+}
+EOS
